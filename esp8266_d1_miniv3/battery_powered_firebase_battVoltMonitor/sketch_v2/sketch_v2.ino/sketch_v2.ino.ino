cf27ff6e6ad2adc4cc30610d574f2ce26ebc047b
mattia46old <mattiacolalongo82@gmail.com>
2025-01-22 12:51:08 +0100

    esp8266 addition

--- esp8266_d1_miniv3/battery_powered_firebase_battVoltMonitor/sketch_v2/sketch_v2.ino/sketch_v2.ino.ino
+++ esp8266_d1_miniv3/battery_powered_firebase_battVoltMonitor/sketch_v2/sketch_v2.ino/sketch_v2.ino.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include "time.h"
 #include<Firebase_ESP_Client.h>
-#define API_KEY "API_KEY_STRING"
-#define DATABASE_URL "firebase_link.app/"
+#define API_KEY "APIKEYinFireBaseDB"
+#define DATABASE_URL "[...]firebasedatabase.app/"
 //Provide the token generation process info.
 // #include "addons/TokenHelper.h"
 // //Provide the RTDB payload printing info and other helper functions.
@@ -10,30 +10,30 @@
 
 // Replace with SSID
 #ifndef STASSID
-#define STASSID "wifi_id"
-#define STAPSK "wifi_psswd"
+#define STASSID "wifiid"
+#define STAPSK "wifipssw"
 #endif
 
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
+int sleepTime = 10e6; //10 seconds in us
 
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 bool signupOK = false;
-int pinVolt = A0;
+int pinVolt = 12;
 float volt;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(pinVolt, INPUT);
+  // pinMode(pinVolt, INPUT);
+  // pinMode(D0, WAKEUP_PULLUP);
   Serial.begin(115200);
-  delay(2000);
-
   // adcVolt = analogRead(pinVolt);
   // volt = adcVolt /(R2/(R1+R2));
-  volt = analogRead(pinVolt) * 5 / 1024; //4.2 max voltage of lipo
+  // volt = analogRead(pinVolt) * 5 / 1024; //4.2 max voltage of lipo
  
  
   // We start by connecting to a WiFi 
@@ -70,7 +70,6 @@ Serial.println();
   }
   else{
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
-    delay(2000);
   }
 
    /* Assign the callback function for the long running token generation task */
@@ -82,29 +81,36 @@ Serial.println();
   
   Serial.print("pin voltage: ");
   Serial.println(volt);
+  delay(1000);
+  ESP.deepSleep(sleepTime); //60e6 --> 60 sec in us times 60 is 1 hour times 1 still 1 hour  
+  // delay(10);
 
 }
 
 
 
 void loop() {
-  if (Firebase.ready() && signupOK){
-    delay(200);
+  // if (Firebase.ready() && signupOK){
+  //   delay(200);
 
-    Firebase.RTDB.getInt(&fbdo, "/counter/"); // this points at FirebaseData. It builds a request that I suppose gets
-    // stored temporarily in the DATABASE SESSION
-    int count = fbdo.intData(); //if the previous line is True then we can ask to the fbdo to print the integer.
-    ++count;
-    Serial.println(count);
-    Firebase.RTDB.setFloat(&fbdo, "ESP8266/" + String(count) + "/batt_volt", volt);
+  //   Firebase.RTDB.getInt(&fbdo, "/counter/"); // this points at FirebaseData. It builds a request that I suppose gets
+  //   // stored temporarily in the DATABASE SESSION
+  //   int count = fbdo.intData(); //if the previous line is True then we can ask to the fbdo to print the integer.
+  //   ++count;
+  //   Serial.println(count);
+  //   Firebase.RTDB.setFloat(&fbdo, "ESP8266/" + String(count) + "/batt_volt", volt);
 
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
+  //   digitalWrite(LED_BUILTIN, HIGH);
+  //   delay(1000);
+  //   digitalWrite(LED_BUILTIN, LOW);
 
-    Firebase.RTDB.setInt(&fbdo, "/counter/", count);
-    delay(2000);
-  }
+  //   Firebase.RTDB.setInt(&fbdo, "/counter/", count);
+  //   delay(2000);
+  // }
+
+  // // ESP.deepsleep(60e6 * 60 * 1) //60e6 --> 60 sec in us times 60 is 1 hour times 1 still 1 hour
+  
+  // ESP.deepSleep(sleepTime); //60e6 --> 60 sec in us times 60 is 1 hour times 1 still 1 hour  
 
 }
 
