649bf6d31d36aad83c971f3c74296e471d424fef
mattia46old <mattiacolalongo82@gmail.com>
2025-06-07 20:53:25 +0200

    modified moist proj

--- moisture_senso/moist_HA/moist_HA.ino
+++ moisture_senso/moist_HA/moist_HA.ino
@@ -6,15 +6,15 @@ include <ESP8266WiFi.h>
 #include "ArduinoJson.h"
 
 
-#define VPIN A0
-#define LEDPIN 2
 
 const char* ssid = "WIFI_ID";
 const char* password = "WIFI_PASS";
 const char* mqtt_server = "192.168.1.235";
-float volt;
 uint64_t time_ms = 60e6;
 //D1 mini in A0 has already voltage divider of 220k/ 100k but the ESP12F module does not!
+int _moisture,sensor_analog;
+const int sensor_pin = A0;  /* Soil moisture sensor O/P pin */
+int sensorON = 12;
 
 
 WiFiClient espClient;
@@ -24,24 +24,26 @@ JsonDocument doc;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LEDPIN, OUTPUT);
-  pinMode(A0, INPUT);
   Serial.begin(74880);
   delay(5000);
+  Serial.println("micro booted up");
+  // turn on sensor is the GPIO pin that turns on the VCC for the capacitor sensor
+  pinMode(sensorON, OUTPUT);
+  digitalWrite(sensorON, HIGH);
 
-  Serial.println("Booted Up");
-
-  // average reading
+  // read sensor analog value --> which is the Aout of the moisture sensor 
+  // average reading (ADC reading has to go before WIFI goes on, otherwise the pin cant be used)
   float tmp = 0;
   for (int i=0; i<10; i++){
-    volt = float(analogRead(VPIN))/1024*3.3*(R1+R2)/(R2); //3.3 max voltage A0 can receive == 1024 analog value
-    tmp = tmp + volt;
+    sensor_analog = analogRead(sensor_pin); // it goes from 1024 when dry ( about 1 volt down to) to 540 when 100% wet
+    int moisture = map(sensor_analog,540,1024,100,0);
+    tmp = tmp + moisture;
   }
-  Serial.print(tmp/10);  
-  Serial.println("V");
+  Serial.print(moisture/10);  
+  Serial.println("%");
   
-  setup_wifi();
 
+  setup_wifi();
   client.setServer(mqtt_server, 1883);
   // client.setCallback(callback); on this esp no need for callback
 
@@ -51,7 +53,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  doc["voltage"] = volt;
+  doc["moisture"] = moisture;
   char buffer[256];
   serializeJson(doc, buffer);
 
@@ -62,35 +64,13 @@ void loop() {
   }
   client.loop();
 
-  client.publish("ESP8266/voltage",  buffer);
+  client.publish("ESP12F/moisture",  buffer);
   delay(2000);
 
   Serial.println("Going to sleep...");
-  ESP.deepSleep(time_ms * 30); //each 30min wake up
+  ESP.deepSleep(time_ms * 60); //each 1h wake up
 
 }
-
-//void callback(char* topic, byte* message, unsigned int length) {
-// Serial.print("Message arrived on topic: ");
-// Serial.print(topic);
-// Serial.print(". Message: ");
-// String messageTemp;
-//
-// for (int i = 0; i < length; i++) {
-//   Serial.print((char)message[i]);
-//   messageTemp += (char)message[i];
-// }
-// Serial.println();
-// Serial.println(messageTemp);
-// if (messageTemp == "ON"){
-//   Serial.println("Lamp is ON");
-//   digitalWrite(LEDPIN, HIGH);
-// }
-// else{
-//   Serial.println("Lamp is OFF");
-//   digitalWrite(LEDPIN, LOW);
-// }
-//}
 
 
 void setup_wifi() {
@@ -120,12 +100,12 @@ void setup_wifi() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266_clientID", "mqtt-user", "mqtt-password")) {
+    if (client.connect("ESP12F_moisture", "mqtt-user", "mqtt-password")) {
       Serial.println("connected");
       // Subscribe
-      client.subscribe("ESP8266/voltage");
+      client.subscribe("ESP12F/moisture");
     } else {
-      Serial.println("Failed to connect. ESP8266 is in deepsleep!");
+      Serial.println("Failed to connect. ESP12F is in deepsleep!");
       // Wait 5 seconds before retrying
       delay(5000);
     }
